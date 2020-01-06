302caa19cfb3a11d095e2f570f9cd2fdbbdb1b76
Karan Kalimuthu <56226319+Karan-Kalimuthu@users.noreply.github.com>
2020-01-06 17:18:55 +0530

    Add project files.

---
+++ ToDoes/Controllers/ToDoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDoes.Models;
+
+namespace ToDoes.Controllers
+{
+    public class ToDoController : Controller
+    {
+        // GET: ToDo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (DBModel db = new DBModel())
+            {
+                List<ToDo> toDos = db.ToDoes.ToList<ToDo>();
+                return Json(new { data = toDos }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
